9d4bbe7ecae8009f39f1f491f02948bb87530df9
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-07-06 19:52:19 +0700

    add status code enum

---
+++ src/utils/status_code.utils.ts
@@ -0,0 +1,11 @@
+export enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    TOO_MANY_REQUESTS = 429,
+    INTERNAL_SERVER = 500,
+}
