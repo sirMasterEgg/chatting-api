51cfa8eccbb885cd2ff5e1eb64bc53cdb237d1fc
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-07-06 22:10:45 +0700

    fix db field naming consistency

---
+++ prisma/migrations/20230706151019_fix_naming_consistency/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `refresh_token` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `refresh_token`,
+    ADD COLUMN `refreshToken` VARCHAR(191) NULL;
