77eaded93dfbe1aca5c9702d52cf097410190670
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-07-08 02:23:55 +0700

    initialize chat file

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth.route";
+import chatRouter from "./chat.route";
 
 const router: Router = Router();
 
 router.use('/auth', authRouter);
+router.use('/chat', chatRouter);
 
 export default router;
